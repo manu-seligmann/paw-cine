4a1e22c474371dff5a1b71dbbe56ff0223617957
manu-seligmann <manuseligmann@hotmail.com>
2022-06-15 11:17:45 -0300

    Added Prisma, DB schema and DB migration

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Programación en ambiente Web - Proyecto final
 
 Este respositorio tiene como fin almacenar todo el proyecto final de la materia "Programación en ambiente Web" de la Universidad Nacional de Luján.
+
---
+++ prisma/migrations/20220615141320_initial/migration.sql
@@ -0,0 +1,116 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "roomType" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "isVisible" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "roomType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "room" (
+    "id" SERIAL NOT NULL,
+    "roomTypeId" INTEGER NOT NULL,
+    "nro" INTEGER NOT NULL,
+    "isVisible" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "room_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "roomSeats" (
+    "roomId" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
+    "numberX" INTEGER NOT NULL,
+    "numberY" INTEGER NOT NULL,
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "roomSeats_pkey" PRIMARY KEY ("roomId","id")
+);
+
+-- CreateTable
+CREATE TABLE "movie" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "isVisible" BOOLEAN NOT NULL,
+
+    CONSTRAINT "movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "movieLanguages" (
+    "movieId" INTEGER NOT NULL,
+    "language" TEXT NOT NULL,
+    "isVisible" BOOLEAN NOT NULL,
+
+    CONSTRAINT "movieLanguages_pkey" PRIMARY KEY ("movieId","language")
+);
+
+-- CreateTable
+CREATE TABLE "show" (
+    "id" SERIAL NOT NULL,
+    "roomId" INTEGER NOT NULL,
+    "movieId" INTEGER NOT NULL,
+    "language" TEXT NOT NULL,
+    "dateTime" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "show_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reservation" (
+    "id" SERIAL NOT NULL,
+    "showId" INTEGER NOT NULL,
+    "clientName" TEXT NOT NULL,
+    "clientEmail" TEXT NOT NULL,
+    "clientPhone" TEXT NOT NULL,
+    "paidAt" TIMESTAMP(3),
+
+    CONSTRAINT "reservation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reservationSeats" (
+    "reservationId" INTEGER NOT NULL,
+    "roomId" INTEGER NOT NULL,
+    "seatId" INTEGER NOT NULL,
+
+    CONSTRAINT "reservationSeats_pkey" PRIMARY KEY ("reservationId","roomId","seatId")
+);
+
+-- AddForeignKey
+ALTER TABLE "room" ADD CONSTRAINT "room_roomTypeId_fkey" FOREIGN KEY ("roomTypeId") REFERENCES "roomType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "roomSeats" ADD CONSTRAINT "roomSeats_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "movieLanguages" ADD CONSTRAINT "movieLanguages_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "show" ADD CONSTRAINT "show_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "show" ADD CONSTRAINT "show_movieId_language_fkey" FOREIGN KEY ("movieId", "language") REFERENCES "movieLanguages"("movieId", "language") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reservation" ADD CONSTRAINT "reservation_showId_fkey" FOREIGN KEY ("showId") REFERENCES "show"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reservationSeats" ADD CONSTRAINT "reservationSeats_roomId_seatId_fkey" FOREIGN KEY ("roomId", "seatId") REFERENCES "roomSeats"("roomId", "id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reservationSeats" ADD CONSTRAINT "reservationSeats_reservationId_fkey" FOREIGN KEY ("reservationId") REFERENCES "reservation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
